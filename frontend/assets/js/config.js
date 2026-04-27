c6a2d5e70689b5783975a7adc6b95db7801800e0
Aayush Soni <soniaayush0044@gmail.com>
2026-04-28 00:04:22 +0530

    Backend api fix

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,5 +1,5 @@
 // assets/js/config.js
 const CONFIG = {
-    BASE_URL: "https://equinox-ce3f.onrender.com/api/"
+    BASE_URL: "https://equinox-ce3f.onrender.com/api"
        //BASE_URL: "http://localhost:8080/api" // for localhost
 };
